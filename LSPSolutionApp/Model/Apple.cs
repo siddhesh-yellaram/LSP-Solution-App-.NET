11b219c262ba7c7280f8b279915382ed582a9464
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:26:35 +0530

    Add files via upload

---
+++ LSPSolutionApp/Model/Apple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPSolutionApp.Model
+{
+    class Apple : Fruit
+    {
+        public string GetColor()
+        {
+            return "Red";
+        }
+    }
+}
---
+++ LSPSolutionApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSPSolutionApp.Model;
+
+namespace LSPSolutionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit f = new Apple();
+            Console.WriteLine("Color of Apple is : " + f.GetColor());
+            f = new Orange();
+            Console.WriteLine("Color of Orange is : " + f.GetColor());
+            Console.ReadLine();
+        }
+    }
+}
